8e78c6fdc0d717587102a444ba0f3af8362fe216
Amber Liu <40771834+reamber@users.noreply.github.com>
2021-04-02 17:29:04 -0400

    Update BestTimeToBuySell.java

--- BestTimeToBuySell.java
+++ BestTimeToBuySell.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit = 0;
-        int current = 0; 
-        for(int i = 1; i < prices.length; i++){
+        int profit = 0; //answer to return
+        int current = 0; //temp holder
+        for(int i = 1; i < prices.length; i++){ //loop through array
             
-            current = Math.max(prices[i] - prices[i-1], current += prices[i] - prices[i-1] );
+            current = Math.max(prices[i] - prices[i-1], current += prices[i] - prices[i-1] ); //compare and pick larger profit. prices[i] = sell time prices[i-1] = buy time
             
             if(profit < current){
-                profit = current;
+                profit = current; //update the answer
             }      
         }
         return profit;
